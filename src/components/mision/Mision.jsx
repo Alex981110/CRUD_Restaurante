66e8236408b9f50412b59b409d5e4ff4f94b666d
Alexis Mtz <alex.mtz873@gmail.com>
2021-04-24 01:56:11 -0500

    [Add](Mision y vision)

--- src/components/mision/Mision.jsx
+++ src/components/mision/Mision.jsx
@@ -6,7 +6,16 @@ const Mision = () => {
   return (
     <div>
       <SideBarMenu/>
-      <h1 className={Styles.title}>Esta es la misión</h1>
+      <h1 className={Styles.title}>Misión</h1>
+      <div className={Styles.container}>
+        <h2>
+          Satisfacer las necesidades gastronómicas de nuestros clientes, 
+          ofreciendo alimentos y servicios con la más alta calidad, 
+          donde se sobrepasen las expectativas de nuestros clientes y ser un 
+          espacio de trabajo que permita la realización personal y el desarrollo
+          de sus colaboradores en el grupo.
+        </h2>
+      </div>
     </div>
   )
 }
--- src/components/vision/Vision.jsx
+++ src/components/vision/Vision.jsx
@@ -5,8 +5,16 @@ import * as Styles from './Vision.module.css'
 const Vision = () => {
   return (
     <div>
-      <SideBarMenu/>
-      <h1 className={Styles.title}>Esta es la visión</h1>
+      <SideBarMenu />
+      <h1 className={Styles.title}>Visión</h1>
+
+      <div className={Styles.container}>
+        <h2>
+          Ser reconocidos por brindar a nuestros clientes sensaciones agradables y momentos felices.<br/>
+          Posicionarnos en el mercado nacional con la comida más saludable y nutritiva de la zona,
+          aumentando con ello la preferencia de nuestros clientes para alcanzar una mayor ventaja competitiva.
+        </h2>
+      </div>
     </div>
   )
 }
